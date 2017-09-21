2e4667050c7c8c9751651771781ddfc6008a9869
thorarinnt <thorarinnt@tmsoftware.is>
2017-09-21 16:00:33 +0000

    Test klasi

---
+++ src/Example.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace Tms;
+
+
+
+class Example
+{
+    public function sayHello()
+    {
+        return 'Hallo!';
+    }
+
+    public function sayGoodbye(){
+        return 'Goodbye';
+    }
+}
