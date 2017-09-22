160ec0713a9bcb2ed41ad82657150ce019595cd5
thorarinnt <thorarinnt@tmsoftware.is>
2017-09-22 13:20:18 +0000

    Bunch of helpers

--- src/helpers.php
+++ src/helpers.php
@@ -68,3 +68,47 @@
  'version' => $version
  );
  }
+
+
+
+
+
+
+ add_action('wp_head', 'wpb_add_googleanalytics');
+ function wpb_add_googleanalytics() {
+ 	 $ex = new Tms\Example();
+ 	 echo $ex->sayHello();
+ }
+
+
+
+
+
+ new Tms\ThemeOptions();
+
+
+ // Helper function to use in your theme to return a theme option value
+ function myprefix_get_theme_option1( $id = '' ) {
+ 	return Tms\ThemeOptions::get_theme_option( $id );
+ }
+
+
+
+ add_action('wp_head', 'wpb_add_googleanalytics1');
+ function wpb_add_googleanalytics1() {
+ 	if( !is_admin() ):
+ 		// til að koma í veg fyrir að pagespeed insights fái scriptuna serveraða til sín
+ 		//if (!isset($_SERVER['HTTP_USER_AGENT']) || stripos($_SERVER['HTTP_USER_AGENT'], 'Speed Insights') === false):
+ 			// your analytics code here
+   		echo '<script>' . myprefix_get_theme_option1('input_example') . '</script>';
+
+
+ 	//	endif;
+ 	endif;
+ }
+
+
+ // An alias to the class
+ function my_excerpt1($length = 55) {
+   Tms\Excerpt::length($length);
+ }
