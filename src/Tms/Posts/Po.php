8c2323a8a82ea98cce149b3c131fc388ef87d206
Þórarinn Ingi Tómasson <1thorarinn@gmail.com>
2017-11-09 13:30:25 +0000

    Added some more test classes with sub packages

---
+++ src/Tms/Posts/Po.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thorarinnt
+ * Date: 09/11/2017
+ * Time: 13:12
+ */
+
+namespace Tms\Posts;
+
+
+class Po {
+
+    function test(){
+        return 'Works';
+    }
+
+}
---
+++ src/Tms/Posts/PostWorker.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thorarinnt
+ * Date: 09/11/2017
+ * Time: 12:36
+ */
+
+namespace Tms\Posts;
+
+
+class PostWorker {
+
+
+    public function sayShit(){
+
+        return 'some shit!';
+
+    }
+
+
+
+
+
+}
